b9193b71d716cf8a29957d6647e379034cb90d51
Huy Tuan <154073309+huyt39@users.noreply.github.com>
2024-09-25 16:36:59 +0700

    first example

---
+++ Lab01/HelloWorld.java
@@ -0,0 +1,13 @@
+package Lab01;
+
+//Example 1: HelloWorld.java
+// Text-printing program
+
+public class HelloWorld{
+
+    public static void main(String args[]){
+        System.out.println("Hello");
+        System.out.println("Ho Tuan Huy");
+        System.out.println("20225856");
+    }
+}
