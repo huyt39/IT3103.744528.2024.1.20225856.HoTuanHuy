d4288d7bbc441ed42c07d87b4f19613f855de20a
Huy Tuan <154073309+huyt39@users.noreply.github.com>
2024-10-02 10:44:07 +0700

    update report

--- Lab01/Calculate.java
+++ Lab01/Calculate.java
@@ -9,7 +9,7 @@ public class Calculate {
                     "Please input the first number: ", "Input the first number", //Hien thi hop thoai de input so thu nhat
                                 JOptionPane.INFORMATION_MESSAGE);
         strNum2 = JOptionPane.showInputDialog(null,
-                    "Please input the first number: ", "Input the first number", //Hien thi hop thoai de input so 2
+                    "Please input the second number: ", "Input the first number", //Hien thi hop thoai de input so 2
                                 JOptionPane.INFORMATION_MESSAGE);
         double num1 = Double.parseDouble(strNum1); //chuyen so thu nhat tu dang string sang double
         double num2 = Double.parseDouble(strNum2); //chuyen so thu 2 tu dang string sang double
