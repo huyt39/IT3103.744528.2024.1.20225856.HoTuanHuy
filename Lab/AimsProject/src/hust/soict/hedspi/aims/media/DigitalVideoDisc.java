59a01f098149ac08a62c2a88ee7d488af056aa42
Huy Tuan <154073309+huyt39@users.noreply.github.com>
2024-12-04 19:37:24 +0700

    update DigitalVideoDisc class

--- Lab/AimsProject/src/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
+++ Lab/AimsProject/src/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
@@ -1,8 +1,11 @@
 package Lab.AimsProject.src.hust.soict.hedspi.aims.media;
-
+//DigitalVideoDisc class represents a DVD and extends the Disc class, implementing the Playable interface
 public class DigitalVideoDisc extends Disc implements Playable {
     //Constructors
     //Constructor for a DVD with id and title
+    public DigitalVideoDisc(String title, String category, String director, float cost){
+        super(title, category, director, cost);
+    }
     public DigitalVideoDisc(int id, String title){super(id, title);}
     //Constructor for a DVD with ID, title, category, and cost
     public DigitalVideoDisc(int id, String title, String category, float cost){
@@ -18,7 +21,7 @@ public class DigitalVideoDisc extends Disc implements Playable {
     }
 
     //Constructor for a DVD with ID, title, category, director, length, and cost
-    public DigitalVideoDisc(int id, String title, String category, String director, float cost, int length){
+    public DigitalVideoDisc(int id, String title, String category, String director, int length, float cost){
         this(id, title, category, director, cost);
         this.setLength(length);
     }
