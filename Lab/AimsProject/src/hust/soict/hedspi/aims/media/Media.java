079dbf2e5a11be94c2ece55d6abfba0dd36ad327
Huy Tuan <154073309+huyt39@users.noreply.github.com>
2024-12-04 19:38:15 +0700

    update Media class

--- Lab/AimsProject/src/hust/soict/hedspi/aims/media/Media.java
+++ Lab/AimsProject/src/hust/soict/hedspi/aims/media/Media.java
@@ -1,5 +1,10 @@
 package Lab.AimsProject.src.hust.soict.hedspi.aims.media;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import Lab.AimsProject.src.hust.soict.hedspi.aims.media.*;
+
+
 public abstract class Media {
     //Attributes
     private int id; //Unique identifier for the media
@@ -7,10 +12,19 @@ public abstract class Media {
     private String category; //Category of the media
     private float cost; //Cost of the media
 
+    public static final Comparator<Media> COMPARE_BY_TITLE_COST = new MediaComparatorByTitleCost();
+    public static final Comparator<Media> COMPARE_BY_COST_TITLE = new MediaComparatorByCostTitle();
+
     //Constructors
     public Media(int id, String title){
         this.id = id;
         this.title = title;
+    }
+
+    public Media(String title, String category, float cost){
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
     }
     
     public Media(int id, String title, String category, float cost){
@@ -35,9 +49,25 @@ public abstract class Media {
     public float getCost(){return cost;}
     public void setCost(float cost){this.cost = cost;}
 
+    public boolean isMatch(String title) {
+        return this.title.equalsIgnoreCase(title); // So sánh không phân biệt chữ hoa/thường
+    }
+
     @Override
     public String toString(){
         return "Media{" + "id=" + id + ", title='" + title + '\'' +
         ", category='" + category + '\'' + ", cost=" + cost + '}';
     }
+
+    @Override
+    public boolean equals(Object o){
+        Media media = (Media) o;
+        try{
+            String title = media.getTitle();
+            return title.equals(this.getTitle());
+        } catch (NullPointerException e){
+            return false;
+        }
+    }
 }
+
