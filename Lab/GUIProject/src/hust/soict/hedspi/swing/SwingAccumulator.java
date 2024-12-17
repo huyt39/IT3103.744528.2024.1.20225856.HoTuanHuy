460b5dbeb99baae3cb4bbe304bc5e6de460bcd5d
Huy Tuan <154073309+huyt39@users.noreply.github.com>
2024-12-17 17:10:33 +0700

    SwingAccumulator class

---
+++ Lab/GUIProject/src/hust/soict/hedspi/swing/SwingAccumulator.java
@@ -0,0 +1,56 @@
+package Lab.GUIProject.src.hust.soict.hedspi.swing;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import java.awt.Container;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Label;
+
+public class SwingAccumulator extends JFrame {
+    private JTextField tfInput;
+    private JTextField tfOutput;
+    private int sum = 0; //Accumulated sum, init to 0
+
+    //Constructor to setup the GUI components and event handlers
+    public SwingAccumulator(){
+        Container cp = getContentPane();
+        cp.setLayout(new GridLayout(2, 2)); //2r and 2c
+
+        cp.add(new JLabel("Enter an integer: "));
+
+        tfInput = new JTextField(10); //10 cols
+        cp.add(tfInput);
+        tfInput.addActionListener(new TFInputListener());
+
+        cp.add(new JLabel("The accumulated sum is: "));
+
+        tfOutput = new JTextField(10); //10 cols
+        tfOutput.setEditable(false);
+        cp.add(tfOutput);
+
+        setTitle("Swing accumulator");
+        setSize(350, 120); //w and h
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new SwingAccumulator();
+    }
+
+    private class TFInputListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent evt){
+            int numberIn = Integer.parseInt(tfInput.getText());
+            sum += numberIn;
+            tfInput.setText("");
+            tfOutput.setText(sum + "");
+        }
+    }
+    
+}
